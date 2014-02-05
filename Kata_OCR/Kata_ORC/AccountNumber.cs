55209075563781f8ff7c378f359e6def8fdc568a
florian.obwegs@hotmail.com <vipertex@gmx.net>
2014-02-05 21:25:48 +0100

    add vs12 kata into github

---
+++ Kata_OCR/Kata_ORC/AccountNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata_OCR
+{
+    class AccountNumber
+    {
+        private Character[] orgData = new Character[9];
+
+        public void addCharacter (Character character)
+        {
+            int arrLength = this.orgData.Length;
+            this.orgData[arrLength + 1] = character;
+        }
+
+        public Character[] getOrgData ()
+        {
+            return this.orgData;
+        }
+    }
+}
---
+++ Kata_OCR/Kata_ORC/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Kata_OCR
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            string file = @"\\192.168.10.2\Nas-Home\Software\Allgemein\Visual Studio\Visual Studio 2012\Kata_OCR\Kata_ORC\SampleData\sample.txt";
+            
+            System.IO.StreamReader sReader = new System.IO.StreamReader(file);
+
+
+            string fileLine;
+            int lineCounter = 0;
+            var combinedNumber = new Dictionary<int, string>() ;
+            
+            while((fileLine = sReader.ReadLine()) != null)
+            {
+                    
+                if(fileLine.Length > 0){
+                    if (lineCounter == 0)
+                    {
+                        AccountNumber accountNumber = new AccountNumber();
+                    }
+                    Console.WriteLine(fileLine + lineCounter);
+                    lineCounter++;
+                }
+                else{
+                    
+                    lineCounter = 0;
+                }
+             
+            }
+            Console.WriteLine(fileLine);                 
+            sReader.Close();
+            Console.Read();
+        }
+
+
+
+        public static string fileLine { get; set; }
+    }
+}
